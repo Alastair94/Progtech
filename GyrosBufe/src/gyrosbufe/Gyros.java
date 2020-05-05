0c8173c3b16047592ddfc7e347bc199d62f6438c
Alastair94 <szondiazazel@gmail.com>
2020-05-05 13:01:12 +0200

    Created abstract Gyros class
    Created an abstract class, based on template method.
    Methods of class:
    - Keszit (public)
    - AlapElkeszit
    - HustHozzaAd
    - ZoldsegetBele
    - SzoszBele
    - Felszolgal

---
+++ GyrosBufe/src/gyrosbufe/Gyros.java
@@ -0,0 +1,30 @@
+package gyrosbufe;
+
+/**
+ *
+ * @author Szondi Máté
+ */
+public abstract class Gyros {
+    public void keszit(){
+        AlapElkeszit();
+        HustHozzaAd();
+        ZoldsegetBele();
+        SzoszBele();
+        Felszolgal();
+    }
+    
+    protected abstract void AlapElkeszit();
+    
+    private void HustHozzaAd(){
+        System.out.println("A húst leszelem a forgatósütőről, majd elhelyezem az előkészített alaphoz.");
+    }
+    
+    protected abstract void ZoldsegetBele();
+    
+    protected void SzoszBele(){};
+    
+    private void Felszolgal(){
+        System.out.println("Az elkészült ételt felszolgálom a vásárlónak.");
+    }
+    
+}
